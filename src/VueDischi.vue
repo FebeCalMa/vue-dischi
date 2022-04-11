f2ed9114bc88c1032fa9f67e24e1666e8bad31b6
Febe Calderon <samcalma1@gmail.com>
2022-04-11 15:52:43 +0200

    cards

---
+++ src/VueDischi.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <LogoSpotify />
+    <img alt="Vue logo" src="./assets/logo.png" />
+  </div>
+</template>
+
+<script>
+import LogoSpotify from "./components/LogoSpotify.vue";
+
+export default {
+  name: "App",
+  components: {
+    LogoSpotify,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/style/variables";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
